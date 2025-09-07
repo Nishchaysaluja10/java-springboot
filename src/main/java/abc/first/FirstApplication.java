926cb04ee3d046398a7df8bb97c01104e9cc0d16
nishchay <nishchaysaluja13@gmail.com>
2025-09-07 23:06:18 +0530

    1st commit

--- src/main/java/abc/first/FirstApplication.java
+++ src/main/java/abc/first/FirstApplication.java
@@ -9,7 +9,7 @@ public class FirstApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(FirstApplication.class, args);
 		System.out.println("Hello World");
-		System.out.println("Welcome to Java Full Stack Development");
+		System.out.println("Welcome to Java");
 	}
 
 }
