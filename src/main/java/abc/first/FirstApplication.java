9fd515c347ce17d082ee3f9bc14aa3b1a599deea
nishchay <nishchaysaluja13@gmail.com>
2025-09-07 23:02:43 +0530

    initial commit

---
+++ src/main/java/abc/first/FirstApplication.java
@@ -0,0 +1,16 @@
+package abc.first;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstApplication.class, args);
+		System.out.println("Hello World");
+		System.out.println("Welcome to Java Full Stack Development");
+	}
+
+}
+
---
+++ src/main/java/abc/first/HomeController.java
@@ -0,0 +1,12 @@
+package abc.first;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+    @GetMapping("/")
+    public String home() {
+        return "Hello, Spring Boot is runnin23423";
+    }
+}   
